870a32ca285513ab6894766e6f1a7570f1e9b5c5
Svetlana Marhefka <sm269@njit.edu>
2020-06-19 16:58:06 -0400

    Dockerfile - added run pip install openpyxl Created an index.html file Created a python file to upload excel files.

--- django-base/IS601/settings.py
+++ django-base/IS601/settings.py
@@ -36,7 +36,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',]
+    'django.contrib.staticfiles',
+    'sampleExcel.apps.SampleexcelConfig',
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- django-base/sampleExcel/urls.py
+++ django-base/sampleExcel/urls.py
@@ -13,11 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
+
+
 
 urlpatterns = [
-    path('charmsapp/', include('charmsapp.urls')),
-    path('sampleExcel/', include('sampleExcel.urls')),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
