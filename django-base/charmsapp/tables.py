bf0184ec83795c49d54c2996ae3fe69656f0d4f3
Svetlana Marhefka <sm269@njit.edu>
2020-07-08 00:08:06 -0400

    Branching off of master again because I implemented another way of creating the data table for the review page. - In line editing is possible (just need help with ajax) - Am able to filter information (currenctly just on favorite number)
    Need help on:
    - Is there a dymic way of printing out the column headers in the table?

--- django-base/charmsapp/tables.py
+++ django-base/charmsapp/tables.py
@@ -1,9 +1,6 @@
 import django_tables2 as tables
 from .models import SampleData
 
-
-from django.db.models.functions import Length
-
 class SampleDataTable(tables.Table):
     class Meta:
         model = SampleData
